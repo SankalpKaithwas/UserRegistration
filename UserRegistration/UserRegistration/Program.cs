428ec854fcbdc5b92e9cddf3ba8a8fca1ef4df6d
unknown <kaithwas.697@gmail.com>
2022-03-31 10:53:20 +0530

    Added feature to validate first name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationRegex userRegistration = new UserRegistrationRegex();
+            Console.WriteLine("Enter your first Name: ");
+            string input = Console.ReadLine();
+            userRegistration.ValidLastName(input);
+
+        }
+    }
+}
